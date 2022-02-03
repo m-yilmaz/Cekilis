69ca9600bbf70af4c6f6e3279ad4580937454263
m-yilmaz <yilmazmustafa93@gmail.com>
2022-02-03 17:14:52 +0300

    Add project files.

---
+++ Cekilis/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Cekilis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAdlar = new System.Windows.Forms.ListBox();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnCekilis = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnYukari = new System.Windows.Forms.Button();
+            this.btnAsagi = new System.Windows.Forms.Button();
+            this.chkKaldir = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lstAdlar
+            // 
+            this.lstAdlar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstAdlar.FormattingEnabled = true;
+            this.lstAdlar.Location = new System.Drawing.Point(28, 45);
+            this.lstAdlar.Name = "lstAdlar";
+            this.lstAdlar.Size = new System.Drawing.Size(228, 329);
+            this.lstAdlar.TabIndex = 2;
+            this.lstAdlar.SelectedIndexChanged += new System.EventHandler(this.lstAdlar_SelectedIndexChanged);
+            this.lstAdlar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lstAdlar_KeyDown);
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(25, 23);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(20, 13);
+            this.lblAd.TabIndex = 1;
+            this.lblAd.Text = "Ad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(61, 19);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(115, 20);
+            this.txtAd.TabIndex = 0;
+            this.txtAd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAd_KeyDown);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(182, 19);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(74, 20);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnCekilis
+            // 
+            this.btnCekilis.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCekilis.Location = new System.Drawing.Point(28, 425);
+            this.btnCekilis.Name = "btnCekilis";
+            this.btnCekilis.Size = new System.Drawing.Size(228, 23);
+            this.btnCekilis.TabIndex = 3;
+            this.btnCekilis.Text = "Çekiliş Yap";
+            this.btnCekilis.UseVisualStyleBackColor = true;
+            this.btnCekilis.Click += new System.EventHandler(this.btnCekilis_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSonuc.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.ForeColor = System.Drawing.Color.Lime;
+            this.lblSonuc.Location = new System.Drawing.Point(362, 45);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(386, 403);
+            this.lblSonuc.TabIndex = 5;
+            this.lblSonuc.Text = "?";
+            this.lblSonuc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnYukari
+            // 
+            this.btnYukari.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnYukari.BackgroundImage = global::Cekilis.Properties.Resources.up_arrow;
+            this.btnYukari.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnYukari.Location = new System.Drawing.Point(274, 150);
+            this.btnYukari.Name = "btnYukari";
+            this.btnYukari.Size = new System.Drawing.Size(68, 40);
+            this.btnYukari.TabIndex = 6;
+            this.btnYukari.UseVisualStyleBackColor = true;
+            this.btnYukari.Click += new System.EventHandler(this.btnYukari_Click);
+            // 
+            // btnAsagi
+            // 
+            this.btnAsagi.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnAsagi.BackgroundImage = global::Cekilis.Properties.Resources.down_arrow;
+            this.btnAsagi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAsagi.Location = new System.Drawing.Point(274, 211);
+            this.btnAsagi.Name = "btnAsagi";
+            this.btnAsagi.Size = new System.Drawing.Size(68, 38);
+            this.btnAsagi.TabIndex = 7;
+            this.btnAsagi.UseVisualStyleBackColor = true;
+            this.btnAsagi.Click += new System.EventHandler(this.btnAsagi_Click);
+            // 
+            // chkKaldir
+            // 
+            this.chkKaldir.AutoSize = true;
+            this.chkKaldir.Location = new System.Drawing.Point(28, 392);
+            this.chkKaldir.Name = "chkKaldir";
+            this.chkKaldir.Size = new System.Drawing.Size(163, 17);
+            this.chkKaldir.TabIndex = 8;
+            this.chkKaldir.Text = "Çekiliste çıkanı listeden kaldır";
+            this.chkKaldir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 460);
+            this.Controls.Add(this.chkKaldir);
+            this.Controls.Add(this.btnAsagi);
+            this.Controls.Add(this.btnYukari);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnCekilis);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.lstAdlar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Çekiliş";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstAdlar;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnCekilis;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnYukari;
+        private System.Windows.Forms.Button btnAsagi;
+        private System.Windows.Forms.CheckBox chkKaldir;
+    }
+}
+
---
+++ Cekilis/Form1.cs
@@ -0,0 +1,163 @@
+﻿using Cekilis.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cekilis
+{
+    public partial class Form1 : Form
+    {
+        List<string> adlar;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            Icon = Resources.cekilis;
+            VerileriOku();
+            Listele();
+        }
+        private void VerileriOku()
+        {
+            try
+            {
+                adlar = File.ReadAllLines("adlar.txt").ToList();
+            }
+            catch (Exception)
+            {
+                adlar = new List<string>();
+            }
+        }
+        void Karistir(List<string> liste)
+        {
+            int talihliIndex;
+            string gecici;
+            for (int i = 0; i < liste.Count; i++)
+            {
+                talihliIndex = rnd.Next(i, liste.Count);
+                gecici = liste[i];
+                liste[i] = liste[talihliIndex];
+                liste[talihliIndex] = gecici;
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            int sid = lstAdlar.SelectedIndex;
+            // Hata kontrolü
+            string ad = txtAd.Text.Trim();
+            if (ad == "")
+            {
+                MessageBox.Show("Eklemek için bir isim girmelisiniz.");
+                return;
+            }
+            // Verinin eklenme
+            adlar.Add(ad);
+
+            // formun temizlenmesi
+            txtAd.Clear();
+            txtAd.Focus();
+            // Listbox güncelleme
+            Listele();
+            lstAdlar.SelectedIndex = sid;
+        }
+
+        private void Listele()
+        {
+            lstAdlar.Items.Clear();
+            foreach (string item in adlar)
+                lstAdlar.Items.Add(item);
+        }
+
+        private void txtAd_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Text Box'a veri girdikten sonra Enter'a basınca btnEkle click oluyor.
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // tuşa basılmamış gibi algıla demektir.
+                btnEkle.PerformClick();
+            }
+        }
+
+        private void btnCekilis_Click(object sender, EventArgs e)
+        {
+            if (adlar.Count == 0) return;
+            Karistir(adlar);
+            lblSonuc.Text = adlar[0];
+            if (chkKaldir.Checked)
+                adlar.RemoveAt(0);
+            Listele();
+        }
+
+        private void lstAdlar_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Hiçbirşey seçili değilse, selected index = -1;
+            int sid = lstAdlar.SelectedIndex;
+            if (e.KeyCode == Keys.Delete && sid > -1)
+            {
+                adlar.RemoveAt(sid);
+                Listele();
+                lstAdlar.SelectedIndex = sid < lstAdlar.Items.Count ? sid : lstAdlar.Items.Count - 1;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Program Kapatılacaktır. Değişiklikleri kaydetmek istiyor musunuz ?", "Çekiliş", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button3);
+            switch (dr)
+            {
+                case DialogResult.Cancel:
+                    e.Cancel = true;
+                    break;
+                case DialogResult.Yes:
+                    File.WriteAllLines("adlar.txt", adlar);
+                    break;
+                case DialogResult.No:
+                    break;
+            }
+        }
+
+        private void btnYukari_Click(object sender, EventArgs e)
+        {
+            string temp;
+            int tasinacakKisi = lstAdlar.SelectedIndex;
+            if (lstAdlar.SelectedIndex != 0)
+            {
+                temp = adlar[tasinacakKisi];
+                adlar[tasinacakKisi] = adlar[tasinacakKisi - 1];
+                adlar[tasinacakKisi - 1] = temp;
+                Listele();
+                lstAdlar.SelectedIndex = tasinacakKisi - 1;
+            }
+            else return;
+        }
+
+        private void btnAsagi_Click(object sender, EventArgs e)
+        {
+            string temp;
+            int tasinacakKisi = lstAdlar.SelectedIndex;
+
+            if (lstAdlar.SelectedIndex != adlar.Count - 1)
+            {
+                temp = adlar[tasinacakKisi];
+                adlar[tasinacakKisi] = adlar[tasinacakKisi + 1];
+                adlar[tasinacakKisi + 1] = temp;
+                Listele();
+                lstAdlar.SelectedIndex = tasinacakKisi + 1;
+            }
+            else return;
+        }
+
+        private void lstAdlar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnYukari.Enabled = lstAdlar.SelectedIndex > 0;
+            btnAsagi.Enabled = lstAdlar.SelectedIndex != lstAdlar.Items.Count - 1;
+        }
+    }
+}
